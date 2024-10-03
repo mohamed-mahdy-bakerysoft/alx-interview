45c05f265631695f6343d536ee2ef4528a215b05
NdiranguAndrew43 <andymurunga@gmail.com>
2024-10-03 17:47:19 +0300

    Pulling to update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle.py
+"""
 
+
+def pascal_triangle(n:int):
+    """
+    This is a function to plot a designated Pascal Triangle,
+
+    @n: rows in Pascal Triangle
+    """
+    pasCal = [[1]] # The base case with one value in the row
+
+    if n > 0:
+        for i in range(n - 1):
+            interArray = [0] + pasCal[-1] + [0] 
+            newRow = []
+            for j in range(len(pasCal[-1]) + 1):
+                newRow.append(interArray[j] + interArray[j + 1])
+            pasCal.append(newRow)
+        return pasCal
